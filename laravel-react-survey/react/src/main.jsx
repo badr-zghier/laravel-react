8213fada94d71b72848e649aba38090eb36b4bba
BADR <badralzghier@gmail.com>
2024-05-21 18:20:46 +0300

    reslove conflicts issues

--- laravel-react-survey/react/src/main.jsx
+++ laravel-react-survey/react/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./App.css";
 import router from "./router.jsx";
 import { RouterProvider } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
